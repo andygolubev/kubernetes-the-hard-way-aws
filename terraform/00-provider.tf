b97a79182758cf7f35c1ebbc5c20a152da7aaf89
Andy Golubev <verkomf@gmail.com>
2023-09-23 19:59:34 +0300

    refactor terraform provider

--- terraform/00-provider.tf
+++ terraform/00-provider.tf
@@ -1,4 +1,16 @@
 terraform {
+  backend "s3" {
+    #
+    # Do not edit "us-east-1" value.
+    # It will be replaced in the pipeline by the command: sed -i 's/us-east-1/$AWS_REGION/g' 00-provider.tf 
+    #
+
+    bucket         = "terraform-state-for-kubernetes-the-hard-way-packer"
+    key            = "terraform-state-for-kubernetes-the-hard-way-packer/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-state-for-terraform-state-for-kubernetes-the-hard-way-packer"
+    encrypt        = true
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
