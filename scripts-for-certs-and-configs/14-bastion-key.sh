e64dd9f5ba41b7c9b8349f566b7d9dfb3a9d75d3
Andy Golubev <verkomf@gmail.com>
2023-09-30 19:16:49 +0300

    fix ssh-keygen

--- scripts-for-certs-and-configs/14-bastion-key.sh
+++ scripts-for-certs-and-configs/14-bastion-key.sh
@@ -1,6 +1,6 @@
 cd /tmp/kthw-certs
 
-ed25519 -b 4096 -C "account@gmail.com" -N ""  -f bastion-key
+ssh-keygen -t ed25519 -b 4096 -C "account@gmail.com" -N ""  -f bastion-key
 
 # Your identification has been saved in bastion-key
 # Your public key has been saved in bastion-key.pub
